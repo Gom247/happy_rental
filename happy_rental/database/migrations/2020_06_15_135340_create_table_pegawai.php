281797189a2112a8e2045eb503d3f44c7c2bc0f4
Gom247 <ilhamhendroprasetyo@gmail.com>
2020-06-15 20:58:22 +0700

    pembuatan schema table pegawai

---
+++ happy_rental/database/migrations/2020_06_15_135340_create_table_pegawai.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePegawai extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('jenis_kelamin');
+            $table->string('no_tlp');
+            $table->text('alamat');
+            $table->string('jabatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
